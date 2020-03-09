9fedc312e4321bbf9d40bd2fee611377a3cc0e5f
Gheorghe Tarcea <tarcea@gmail.com>
2020-03-09 12:48:37 +0100

    answer and ask created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answers', as: :answer
+  # I wrongly named the file answers instead of answer...
 end
+
+
+get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+
+  # Generic syntax:
+  # verb 'path', to: 'controller#action', as: :route_name
